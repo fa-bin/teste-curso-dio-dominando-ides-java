c509306b54753ff0221ecfba4afdfc8a53421d86
Fabio Mendonca <f_mendonca@outlook.com.br>
2021-10-27 19:39:09 -0300

    Primeiro commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,22 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	Gato gato = new Gato();
+
+	
+	
+	}
+
+
+
+
+
+
+/*public static void main(String[] args) {
+String a = "Hello";
+String b = "World";
+System.out.printf("%s %s", a, b);
+*/
